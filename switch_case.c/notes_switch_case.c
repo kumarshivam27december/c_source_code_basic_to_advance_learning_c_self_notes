8cffd35984d2bc4dcf6dfdedabff871b07ad92e4
Shivam Kumar <kumarshivam16062004@gmail.com>
2023-02-04 00:02:49 +0530

    version 1.0

---
+++ switch_case.c/notes_switch_case.c
@@ -0,0 +1,11 @@
+//Points to remember (Interview questions for Switch case in C)
+
+
+
+/*switch() can only contain char and int.
+break is used to exit from switch statement. It is optional.
+switch case can be without default case.
+A char variable is always initialized within single quotes.
+The expression provided in switch should result in a constant value otherwise it will be invalid.
+The case values should be distinct.
+We can also nest switch statements.*/
---
+++ using_gets()_function.c
@@ -0,0 +1,15 @@
+// why the hell this code is not working
+#include<stdio.h>
+
+int main()
+{
+    printf("\n\n\t\tStudytonight - Best place to learn\n\n\n");
+
+    char str[50];   // char array of size 50
+    printf("Enter your complete name:\n\n\n");
+
+    gets(str);
+    printf("\n\nWelcome to Studytonight  %s\n\n\n", str);
+    printf("\n\n\t\t\tCoding is Fun !\n\n\n");
+    return 0;
+}
